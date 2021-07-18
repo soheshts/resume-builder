9a176c3f6b863d4a475d61b6f014bf4364732265
Sohesh TS <soheshts@gmail.com>
2021-07-18 20:24:16 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    console.log("ready!");
+    initDialog();
+    datepicker = $(function () {
+        $(".datepicker").datepicker();
+    });
+});
+
+function initDialog() {
+    dialog = $("#dialogPersonal").dialog({
+        title: "Personal Details",
+        autoOpen: true,
+        height: 450,
+        width: 400,
+        draggable: false,
+        dialogClass: "no-close",
+        closeOnEscape: false,
+        buttons: {
+            "Next": function () {
+                dialog.dialog("close");
+            },
+            Cancel: function () {
+                dialog.dialog("close");
+            }
+        },
+
+    });
+}
