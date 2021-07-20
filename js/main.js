d08d136c2dfe6eefd42837d47888029c0706b810
Sohesh TS <soheshts@gmail.com>
2021-07-20 19:11:02 +0530

    added educational details, changed to UIKit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
     console.log("ready!");
+    $("#educationalDetails").hide();
+
     
 });
 var userData = new Object();
+userData.educational = []
 function addPersonalData() {
+    $("#personalDetails").hide();
+    $("#educationalDetails").show();
     console.log($("#name").val());
     var personal = new Object();
     personal.name = $("#name").val();
@@ -16,4 +21,23 @@ function addPersonalData() {
     personal.website = $("#website").val();
     userData.personal = personal;
     console.log(JSON.stringify(userData));
+}
+function addEducationalData() {
+    
+    var educational = new Object();
+    educational.name = $("#collegeName").val();
+    educational.course = $("#course").val();
+    educational.from = $("#collegeFromDate").val();
+    educational.to = $("#collegeEndDate").val();
+    
+    userData.educational.push(educational);
+    console.log(JSON.stringify(userData));
+}
+function clearEducationalFields() {
+    addEducationalData();
+    $("#collegeName").val('');
+    $("#course").val('');
+    $("#collegeFromDate").val('');
+    $("#collegeEndDate").val('');
+    
 }
